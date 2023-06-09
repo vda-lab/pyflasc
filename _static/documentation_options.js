ffe55be7f560454f431d49b8b0914dcfcc43ef09
GitHub Action <action@github.com>
2023-06-09 14:46:25 +0000

    Add changes for d79ddb63da20a6a369be939d2629797519f2a4b1

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v0.1.0-3-g8702a8e',
+    VERSION: 'v0.1.0-4-gd79ddb6',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
